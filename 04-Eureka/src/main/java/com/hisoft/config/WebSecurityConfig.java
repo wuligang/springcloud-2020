086e5344ae9e9e4ab15371d5e2be524cfb41ebb7
wuligang <wlggyh@163.com>
2020-10-03 16:50:38 +0800

    Eureka高可用

---
+++ 04-Eureka/src/main/java/com/hisoft/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.hisoft.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @program: springcloud
+ * @description:
+ * @author: wlg
+ * @create: 2020-10-02 13:25:28
+ **/
+@EnableWebSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
